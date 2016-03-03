fe43ef376e39c459829d22c625e85d4e37fd9f27
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-03-03 14:17:34 -0500

    optimize home style

--- shared/components/home/CenterField.jsx
+++ shared/components/home/CenterField.jsx
@@ -6,7 +6,6 @@ import SubmitButton from './SubmitButton';
 import Colors from 'material-ui/lib/styles/colors';
 
 const centerStyle = {
-  height: '10em',
   marginTop: '3%',
   textAlign: 'center',
 };
@@ -21,19 +20,23 @@ const containerStyle = {
   paddingTop: '0.5em'
 };
 
+const inputStyle = {
+  paddingBottom: '1em'
+};
+
 function CenterField () {
   return (
     <div className="row center-xs">
-      <Paper className="col-md-6" style={centerStyle} zDepth={3}>
+      <Paper className="col-md-6 col-xs-10" style={centerStyle} zDepth={3}>
         <div style={containerStyle}>
           <div className="row center-xs">
             <p style={titleStyle}>Find Your Perfect Schedule Now</p>
           </div>
           <div className="row center-xs middle-xs">
-            <div className="col-md-4 col-xs-12">
+            <div style={inputStyle} className="col-md-4 col-xs-12">
               <AutoSuggest school="marianopolis" inputType="school" />
             </div>
-            <div className="col-md-4 col-xs-12">
+            <div style={inputStyle} className="col-md-4 col-xs-12">
               <SubmitButton />
             </div>
           </div>
