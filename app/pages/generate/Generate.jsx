9ab413f09b11ee62b3d798580082b22279c4d6f7
Lawrence Pan <lawrencefeipan@gmail.com>
2016-03-31 18:24:49 -0400

    add generate page title bar and button

--- app/pages/generate/Generate.jsx
+++ app/pages/generate/Generate.jsx
@@ -2,17 +2,30 @@ import React, { PropTypes, Component } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import Paper from 'material-ui/lib/paper';
+import { blue500 } from 'material-ui/lib/styles/colors';
+
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 
 const barStyle = {
   width: '100%',
-  height: '4em',
+  height: '3em',
+  marginLeft: '0',
+  position: 'relative',
+};
+
+const btnStyle = {
+  position: 'absolute',
+  right: '4em',
+  bottom: '-1em',
+};
+
+const titleStyle = {
+  marginLeft: '2em',
+  color: blue500,
 };
 
 class Generate extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   componentWillMount() {
     if (Object.keys(this.props.school).length === 0) {
@@ -22,9 +35,14 @@ class Generate extends Component {
 
   render() {
     return (
-      <Paper style={barStyle} zDepth={3}>
-        <p>{this.props.school.full}</p>
-      </Paper>
+      <div>
+        <Paper className="row middle-xs" style={barStyle} zDepth={3}>
+          <p style={titleStyle}>{this.props.school.full}</p>
+          <FloatingActionButton secondary style={btnStyle}>
+            <ContentAdd />
+          </FloatingActionButton>
+        </Paper>
+      </div>
     );
   }
 }
