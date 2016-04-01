671f1053c36d8cb472b92d6bb009e51a6c9365bc
Lawrence Pan <lawrencefeipan@gmail.com>
2016-03-31 20:00:01 -0400

    add modal with course codes autocomplete

---
+++ app/pages/generate/InputGrid.jsx
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Table from 'material-ui/lib/table/table';
+import TableBody from 'material-ui/lib/table/table-body';
+
+class InputGrid extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Table>
+          <TableBody displayRowCheckbox={false} />
+        </Table>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(store) {
+  return {
+    school: store.school,
+  };
+}
+
+InputGrid.propTypes = {
+  school: PropTypes.shape({
+    full: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(InputGrid);
